d879735ac30126fec32ffe3b7619e5bfebb601dd
asankulov-t <asankulov-t>
2023-09-28 21:37:02 +0600

    response

--- src/Components/Sessions/Sessions.tsx
+++ src/Components/Sessions/Sessions.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import {useSelector} from "react-redux";
 import {RootState} from "../../Store/Store";
 import SessionItem from "./SessionItem/SessionItem";
-import style from "./Sessions.module.css";
 
 const Sessions = () => {
     let movies = useSelector<RootState>(state => state.rootReducer.movies)
     return (
-        <div className={style.container}>
-            <h3 className={'title'}>Расписание на сегодня</h3>
+        <div>
+            <h3 >Расписание на сегодня</h3>
             {
                 // @ts-ignore
                 movies && movies.map((t) => <SessionItem
